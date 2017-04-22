deffd03ab6e54d6b6cab8dbde46e95d478d252b3
Amerie Lommen <ameriel@msn.com>
2017-04-21 19:05:48 -0700

    cleanup

--- purple/homepagescripts.js
+++ purple/homepagescripts.js
@@ -1,15 +1,13 @@
 $(document).ready(function() {
 	$("div").mouseenter(function() {
 		if ($(window).width() > 600) {
-			$(this).children().children("img").fadeTo("slow", 1.0);
-			//$(this).children().children("p").css({ "display": "block", "opacity": "0" }).animate({ "opacity": "1" }, 300);
+			$(this).children().children("img").fadeTo("medium", 1.0);
 		}
 		$(this).children().children("p").css({ "display": "block", "opacity": "0" }).animate({ "opacity": "1" }, 300);
 	});
 	$("div").mouseleave(function() {
 		if ($(window).width() > 600) {
 			$(this).children().children("img").fadeTo("medium", 0.4);
-			//$(this).children().children("p").css("display","none");
 		}
 		$(this).children().children("p").css("display","none");
 	});
